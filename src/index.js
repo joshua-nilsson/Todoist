23064dcca0a9623bab6c113a4519aa0414c0a5cd
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-05-31 09:24:22 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import './App.scss';
+import 'react-toggle/style.css';
 
 render(<App />, document.getElementById('root'));
